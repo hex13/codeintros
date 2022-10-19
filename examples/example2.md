d4aaff00749bcff908f8b56fc828256461612df3
Łukasz Lityński <lukasz.litynski@gmail.com>
2022-10-19 02:18:00 +0200

    example with canvas; screenshots

---
+++ examples/example2.md
@@ -0,0 +1,7 @@
+You can draw on canvas:
+
+{{ snippets.canvas | safe }}
+
+And it will look like this:
+
+![image](canvas.jpg)
--- screenshots.js
+++ screenshots.js
@@ -2,14 +2,16 @@ import puppeteer from 'puppeteer';
 import * as Path from 'node:path';
 
 (async () => {
+    const fileIn = process.argv[2];
+    const fileOut = process.argv[3];
     const browser = await puppeteer.launch();
     console.log("launched");
     const page = await browser.newPage();
     console.log("new page");
-    const url = 'file://' + Path.join(process.cwd(), 'build', 'index.html');
+    const url = 'file://' + Path.join(process.cwd(), fileIn);
     await page.goto(url);
     console.log("page loaded");
-    const screenshot = await page.screenshot({path: 'build/a.jpg'});
+    const screenshot = await page.screenshot({path: fileOut});
     console.log(screenshot)
     await browser.close();
 })();
